e26c91b92df4deb39b98b97d4295760272c65cfc
wynncollie <wynn_collie@outlook.com>
2024-12-06 14:45:27 +0100

    Programación estructurada en Kotlin

--- src/main/java/dev/wynncollie/pruebasjavaykotlin/prog_estructurada/EstructuradaKotlin.kt
+++ src/main/java/dev/wynncollie/pruebasjavaykotlin/prog_estructurada/EstructuradaKotlin.kt
@@ -1,2 +1,43 @@
 package dev.wynncollie.pruebasjavaykotlin.prog_estructurada
 
+fun main() {
+    // Declaración de variables
+    var edad: Int
+    val nombre: String
+    val apellido1: String
+    val apellido2: String
+
+    // Entrada de datos
+    print("Introduzca su nombre: ")
+    nombre = readln()
+    print("Introduzca su primer apellido: ")
+    apellido1 = readln()
+    print("Introduzca su segundo apellido: ")
+    apellido2 = readln()
+    print("Introduzca su edad: ")
+    edad = readln().toInt()
+    // Salida de datos
+    println("Hola $nombre $apellido1 $apellido2, tienes $edad años.")
+
+    if (edad >= 18) {
+        println("Eres mayor de edad.")
+    } else {
+        println("Eres menor de edad.")
+    }
+    println()
+
+    // Bucle while
+    println("Cuenta atrás desde tu edad: $edad")
+    while (edad != 0) {
+        println(edad)
+        edad--
+    }
+    println()
+    // Bucle for
+    println("Imprimiremos los caracteres de tu nombre")
+    for (char in nombre) {
+        println(char)
+    }
+    println()
+    println("Programa finalizado")
+}
